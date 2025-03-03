700cc0fbc687af02b222b40e806c86c152671712
mishabvibes <mdmishab124@gmail.com>
2025-03-03 12:06:01 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-<<<<<<< HEAD
 import { AuthProvider } from "./context/AuthContext";
-=======
->>>>>>> master
 import Footer from "@/components/Footer";
 
 const geistSans = Geist({
