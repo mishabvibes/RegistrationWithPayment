abcc278db502d8fd7bbc461f3667208f4ace93bb
mishabvibes <mdmishab124@gmail.com>
2025-03-03 08:11:41 +0530

    change in india tAlathurpadi Daro Alathurpadi Dars

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -201,7 +201,7 @@ const Footer = () => {
                                     <svg className="w-5 h-5 text-red-500" fill="currentColor" viewBox="0 0 20 20">
                                         <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd"></path>
                                     </svg>
-                                    <span className="text-sm text-gray-500 ml-2">in India</span>
+                                    <span className="text-sm text-gray-500 ml-2">Alathurpadi Dars</span>
                                 </div>
                             </div>
                         </div>
